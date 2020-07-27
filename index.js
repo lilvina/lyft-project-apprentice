260d3222abbc90b1ce35b23c8e81dc0c1f1c765e
lilvina <davinaleo2007@yahoo.com>
2020-07-26 22:16:43 -0700

    finished lyft project

--- index.js
+++ index.js
@@ -1,5 +1,27 @@
 const express = require('express')
 const server = express()
+const bodyParser = require('body-parser')
+
+server.use(express.json())
+server.use(bodyParser.json())
+
+
+server.post('/test', (req, res) => {
+  const { string_to_cut } = req.body
+
+  let pointer = 0
+  let finalStr = ""
+
+  while(pointer < string_to_cut.length) {
+    if(pointer % 3 === 0 && pointer >= 3) {
+      finalStr += string_to_cut[pointer - 1]
+    }
+    pointer++
+  }
+
+  res.json({ return_string: finalStr })
+
+})
 
 server.listen(3000, () => {
   console.log('\n ** Server running on 3000')
